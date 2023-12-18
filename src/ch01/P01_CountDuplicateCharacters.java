13b2d7c5ba19a776ad6aeaf4906ab3bc2c9459ed
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-18 16:59:14 +0900

    [001] 첫 번째 해법에서 유니코드 문자 처리 가능하도록

--- src/ch01/P01_CountDuplicateCharacters.java
+++ src/ch01/P01_CountDuplicateCharacters.java
@@ -7,26 +7,32 @@ import java.util.stream.Collectors;
 public class P01_CountDuplicateCharacters {
     // 첫 번째 해법 : 문자열 내 문자를 순회하며 Map을 사용해  문자를 키로, 빈도수를 값으로 저장하는 방법
     // 현재 문자가 Map에 없으면 (character, 1)을 추가한다. 현재 문자가 Map에 있으면 (character, occurrences+1)처럼 빈도수를 1증가시킨다.
-//    public Map<Character, Integer> countDuplicateCharacters(String str){
-//        Map<Character, Integer> result = new HashMap<>();
-//
-//        // for(char ch: str.toCharArray()) {..}를 사용해도 된다.
-//        for(int i = 0; i < str.length(); i++){
-//            char ch = str.charAt(i);
-//
-//            result.compute(ch,(k,v)->(v==null)? 1:++v);
-//        }
-//        return result;
-//    }
+    public static Map<Character, Integer> countDuplicateCharacters(String str){
+        Map<Character, Integer> result = new HashMap<>();
 
-    // 두 번째 해법 : 자바 8의 스트림 기능 활용
-    public static Map<Character, Long> countDuplicateCharacters(String str){
-        Map<Character, Long> result = str.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
+        for (int i = 0; i < str.length(); i++) {
+            int cp = str.codePointAt(i);
+            char ch = Character.toChars(cp)[0];
+
+            if (Character.charCount(cp) == 2) { // 2는 대리쌍을 뜻함
+                i++;
+            }
+
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
 
         return result;
     }
+
+    // 두 번째 해법 : 자바 8의 스트림 기능 활용
+//    public static Map<Character, Long> countDuplicateCharacters(String str){
+//        Map<Character, Long> result = str.chars()
+//                .mapToObj(c -> (char) c)
+//                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
+//
+//        return result;
+//    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -35,7 +41,7 @@ public class P01_CountDuplicateCharacters {
         String inputString = scanner.nextLine();
 
         // countDuplicateCharacters 메서드를 호출하여 중복 문자의 빈도수를 계산합니다.
-        Map<Character, Long> result = countDuplicateCharacters(inputString);
+        Map<Character, Integer> result = countDuplicateCharacters(inputString);
 
         // 결과를 출력합니다.
         System.out.println("각 문자의 빈도수:");
