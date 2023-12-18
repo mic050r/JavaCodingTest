db17005cb359077dc1ed4572c49b660a2819d932
林지현 <103114387+mic050r@users.noreply.github.com>
2023-12-18 16:44:18 +0900

    [001] 두 번째 해법 : stream 활용 (#6)
    * [001] 문자 개수 세기
    
    * [001] 첫 번째 해법 : Map사용하기
    
    * [001] 메인 추가
    
    * [001] 두 번째 해법 : stream 활용

--- src/ch01/P01.java
+++
@@ -1,9 +0,0 @@
-package ch01;
-import java.util.Scanner;
-public class P01 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); System.out.print("문자입력 : ");
-        String str = sc.nextLine();
-        System.out.println("주어진 문자열의 문자 개수 : " + str.length());
-    }
-}
