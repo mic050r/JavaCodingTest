9f82658906183de3f92c2f96f430634835084874
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-19 14:04:58 +0900

    [002] 세 번째 해법 : 스트림 이용하여 유티코드 대리 쌍을 모두 지원

--- src/ch01/P02_firstNonRepeatedCharacter.java
+++ src/ch01/P02_firstNonRepeatedCharacter.java
@@ -2,11 +2,12 @@ package ch01;
 // 주어진 문자열에서 반복되지 않는 첫 번째 문자를 반환하는 프로그램을 작성하라.
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class P02_firstNonRepeatedCharacter {
 
-    private static final int EXTENDD_ASCII_CODES = 256;
-    public static Character main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,26 +15,20 @@ public class P02_firstNonRepeatedCharacter {
         System.out.print("문자열을 입력하세요: ");
         String str = scanner.nextLine();
 
-        // 두번째 해법
-        // 각 문자마다 문자열을 순회하며 빈도수를 세고, 문자가 중복이면 루프를 종료하고 다음 문자로 넘어가도록
-        // LinkedHashMap 이용
+        // 세 번째 해법 : 스트림 사용
+        Map<Integer, Long> chs = str.codePoints()
+                .mapToObj(cp -> cp)
+                .collect(Collectors.groupingBy(Function.identity(),
+                        LinkedHashMap::new, Collectors.counting()));
 
-        // HashMap 선언
-        LinkedHashMap<Character, Integer> chars = new LinkedHashMap<>();
-        int min = 100000;
+        int cp = chs.entrySet().stream()
+                .filter(e -> e.getValue() == 1L)
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .orElse(Integer.valueOf(Character.MIN_VALUE));
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            // 각 문자의 빈도수 세기
-            chars.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
-        }
+        String result = String.valueOf(Character.toChars(cp));
 
-        for(Map.Entry<Character, Integer> entry : chars.entrySet()){
-            if(entry.getValue() == 1){
-                return entry.getKey();
-            }
-        }
-
-        return Character.MIN_VALUE;
+        System.out.println(result);
     }
 }
