2e6149d1cb2d6724bce95ee377e9c9b2357607d3
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-25 00:57:51 +0900

    [007] 두 번째 해법 : String을 Integer나 Long, Float, Double 객체로 변환하는 방법

--- src/ch01/P07_ConvertStringToNumber.java
+++ src/ch01/P07_ConvertStringToNumber.java
@@ -10,11 +10,17 @@ public class P07_ConvertStringToNumber {
     private static final String TO_DOUBLE = "13.83423D";
 
     public static void main(String args){
-        int toInt = Integer.parseInt(TO_INT);
-        long toLong = Long.parseLong(TO_LONG);
-        float toFloat = Float.parseFloat(TO_FLOAT);
-        double toDouble = Double.parseDouble(TO_DOUBLE);
+//        1. String을 int나 long, float, double로 변환하는 방법
+//        int toInt = Integer.parseInt(TO_INT);
+//        long toLong = Long.parseLong(TO_LONG);
+//        float toFloat = Float.parseFloat(TO_FLOAT);
+//        double toDouble = Double.parseDouble(TO_DOUBLE);
 
+//        2. String을 Integer나 Long, Float, Double 객체로 변환하는 방법
+        Integer toInt = Integer.valueOf(TO_INT);
+        Long toLong = Long.valueOf(TO_LONG);
+        Float toFloat = Float.valueOf(TO_FLOAT);
+        Double toDouble = Double.valueOf(TO_DOUBLE);
 
     }
 }
