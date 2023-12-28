ca32c3e870fbefa3aafe3b282c8baa9333eedb7f
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-29 00:46:52 +0900

    [010] 첫 번째 해법 : 재귀 호출을 이용하여 Set에 저장
    - 리스트로 저장하면 중복 -> Set으로 저장하면 중복 x

--- src/ch01/P10_permuteAndPrint.java
+++ src/ch01/P10_permuteAndPrint.java
@@ -1,36 +1,45 @@
 package ch01;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class P10_permuteAndPrint {
     // 문자열의 순열을 출력하는 메서드
-    public static void permuteAndPrint(String str) {
+    public static Set<String> permuteAndStore(String str) {
         // 초기 호출을 위한 오버로드된 메서드 호출
-        permuteAndPrint("", str);
+        return permuteAndStore("", str);
     }
 
     // 순열을 생성하고 출력하는 재귀적인 도우미 메서드
-    private static void permuteAndPrint(String prefix, String str) {
+    private static Set<String> permuteAndStore(String prefix, String str) {
+        Set<String> permutations = new HashSet<>();
         // 현재 남아 있는 문자열의 길이
         int n = str.length();
 
         // 남아 있는 문자열이 없으면 현재까지의 순열 출력
         if (n == 0) {
-            System.out.print(prefix + " ");
+            permutations.add(prefix + " ");
         } else {
             // 문자열의 각 문자에 대해 재귀 호출을 수행하여 순열 생성
             for (int i = 0; i < n; i++) {
                 // 현재 문자를 prefix에 추가하고 해당 문자를 제외한 나머지 문자열을 재귀 호출로 전달
-                permuteAndPrint(prefix + str.charAt(i), str.substring(i + 1, n) + str.substring(0, i));
+                permutations.addAll(permuteAndStore(prefix + str.charAt(i), str.substring(i + 1, n) + str.substring(0, i)));
             }
         }
+        return permutations;
     }
 
-    // 메인 메서드: 주어진 문자열의 순열을 출력
     public static void main(String[] args) {
         // 원하는 문자열로 변경 가능
         String inputString = "abc";
 
-        // 입력 문자열의 순열을 출력
+        // 주어진 문자열의 순열을 생성하고 저장하는 메서드 호출
+        Set<String> permutations = P10_permuteAndPrint.permuteAndStore(inputString);
+
+        // 결과 출력
         System.out.println("Permutations of " + inputString + ":");
-        P10_permuteAndPrint.permuteAndPrint(inputString);
+        for (String permutation : permutations) {
+            System.out.print(permutation);
+        }
     }
 }
